7f013e07eeb16b1b8ced2703dc026aee16b01305
Unknown <michael.kahle@yahoo.de>
2020-02-27 17:54:05 +0100

    k_772 is example of index.php switching to ruffle if necassary

---
+++ volumes/www/htdocs/common/php/flashes.php
@@ -0,0 +1,59 @@
+<?php
+
+function collectFlashes($moduleDir, $flashDir="flash")
+{
+  $flashes = glob($moduleDir."/".$flashDir."/*_*.swf");
+  return $flashes;
+}
+
+function includeFlashes($flashes) {
+  $i=0;
+  $display = "";
+  foreach($flashes as $flash) {
+    $i++;
+    echo "<div id='page_".$i."' class='page' style='".$display."'>\n";
+    // include($flash);
+	
+    echo " <div class='container-fluid'>\n";
+    echo "  <div class='row' style='margin: 8px;'>\n";
+    echo "   <div class='col-sm-0 col-md-0 col-lg-1 col-xl-2'></div>\n";
+    echo "   <div class='col-sm-12 col-md-11 col-lg-10 col-xl-7'>\n"; 
+    echo "    <object data='".$flash."' \n"
+        ."      classid='clsid:d27cdb6e-ae6d-11cf-96b8-444553540000' \n" 
+        ."      codebase='http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0' \n" 
+        ."      width='950' height='520' \n"
+		."      id='module_page'>\n";
+    echo "     <param name='allowScriptAccess' value='sameDomain'>\n";
+    echo "     <param name='movie' value='".$flash."'>\n";
+    echo "     <param name='quality' value='high'>\n";
+    echo "     <param name='bgcolor' value='#ffffff'>\n";
+    echo "     <embed src='".$flash."' \n" 
+	    ."      quality='high' width='950' height='520' \n" 
+	    ."      bgcolor='#ffffff' name='module_page' \n"
+	    ."      allowscriptaccess='sameDomain' \n" 
+	    ."      type='application/x-shockwave-flash' \n" 
+	    ."      pluginspage='http://www.macromedia.com/go/getflashplayer'>\n";
+    echo "    </object>\n";
+    echo "   </div> <!-- end md-10 -->\n";
+    echo "   <div class='col-sm-0 col-md-1 col-lg-1 col-xl-3'></div>\n";
+    echo "  </div> <!-- end row -->\n";
+    echo " </div> <!-- end container -->\n"; 
+    echo "</div>\n";
+  }
+}
+
+function javascriptFlashes($flashes) {
+  $i=0;
+  $pageIds=[]; 
+  foreach($flashes as $flash) {
+    $i++;
+    $pageIds[]= "'#page_".$i."'";
+  }
+  $js = "pageData = {current:1, pages: [";
+  $js .= implode($pageIds, ",");
+  $js .= "]};";
+  $js .= " initPages();";
+  echo "<script>".$js."</script>\n";
+}
+
+?>
---
+++ volumes/www/htdocs/session.php
@@ -0,0 +1,4 @@
+<?php 
+session_start();
+$_SESSION["flash"] = $_REQUEST["flash"];
+?>
